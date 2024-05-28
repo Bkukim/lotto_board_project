751f273d9e78e663bef35fa8c598f0225530acee
wldudrhdwnsla99 <165975204+wldudrhdwnsla99@users.noreply.github.com>
2024-05-28 13:50:22 +0900

    인덱스

--- boardfrontend/src/router/index.js
+++ boardfrontend/src/router/index.js
@@ -10,32 +10,26 @@ const routes = [
     path: "/about",
     component: () => import("../views/AboutView.vue"),
   },
-  // 공지사항 add
-  {
-    path: "/notice-add",
-    component: () => import("../views/notice/AdminNoticeAdd.vue"),
-  },
   // 로그인
   {
     path: "/member/login",
     component: () => import("../views/auth/LoginView.vue"),
-  },  
-  // 카카오 로그인
+  },
+  // 회원가입 환영 페이지
+  {
+    path: "/member/join/welcome",
+    component: () => import("../views/auth/WelcomeView.vue"),
+  },
+  // 카카오 소셜 로그인
   {
     path: "/auth-redirect",
     component: () => import("../views/auth/KakaoRedirectView.vue"),
-  },  
+  },
   // 회원가입 페이지
   {
     path: "/member/join",
     component: () => import("../views/auth/JoinView.vue"),
   },
-   // 회원가입 환영 페이지
-   {
-    path: "/member/join/welcome",
-    component: () => import("../views/auth/WelcomeView.vue"),
-  },
-
   // 아이디 찾기 페이지
   {
     path: "/member/find-id",
@@ -46,40 +40,61 @@ const routes = [
     path: "/member/find-pwd",
     component: () => import("../views/auth/FindPwd.vue"),
   },
+  // 비밀번호 재설정 페이지
+  {
+    path: "/member/new-pw",
+    component: () => import("../views/auth/NewPwdView.vue"),
+  },
+  // 회원정보 수정 페이지
+  {
+    path: "/member/user-modify",
+    component: () => import("../views/user/UserModifyView.vue"),
+  },
+
   // 공지사항
   {
     path: "/notice/notice-board",
     component: () => import("../views/notice/noticeBoard.vue"),
+  },
+  // 공지사항 add
+  {
+    path: "/notice-add",
+    component: () => import("../views/notice/AdminNoticeAdd.vue"),
   },
   // 자유 게시판
   {
     path: "/free/free-board",
     component: () => import("../views/free/freeBoard.vue"),
   },
-    // 자유 게시판 -> 상세조회
-    {
-      path: "/free/free-boardDetail",
-      component: () => import("../views/free/freeBoardDetail.vue"),
-    },
+  // 자유 게시판 -> 상세조회
+  {
+    path: "/free/free-boardDetail",
+    component: () => import("../views/free/freeBoardDetail.vue"),
+  },
   // 동아리 게시판
   {
-    path: "/club/club-board",
-    component: () => import("../views/club/clubBoard.vue"),
+    path: "/clud/clud-board",
+    component: () => import("../views/clud/cludBoard.vue"),
   },
   // 동아리 게시판 -> 매칭글쓰기
   {
-    path: "/club/club-boardDetail",
-    component: () => import("../views/club/clubBoardDetail.vue"),
+    path: "/clud/clud-boardDetail",
+    component: () => import("../views/clud/cludBoardDetail.vue"),
   },
   // 동아리 게시판 -> 매칭 모집글 게시글
   {
-    path: "/club/club-boardRecruitment",
-    component: () => import("../views/club/clubBorardRecruitment.vue"),
+    path: "/clud/clud-boardRecruitment",
+    component: () => import("../views/clud/cludBorardRecruitment.vue"),
   },
   // 건의 게시판
   {
     path: "/complaint/complaint-board",
     component: () => import("../views/complaint/complaintBoard.vue"),
+  },
+  // 각게시판별 공지사항 관리 ::
+  {
+    path: "/notice-master",
+    component: () => import("../views/admin/MasterBoardNoticesAdmin.vue"),
   },
 ];
 
