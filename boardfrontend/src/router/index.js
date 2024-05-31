32d9bf5cc3f67772036515f283eafc0b7bdb5eb5
Bku <full1569@gmail.com>
2024-05-31 17:13:08 +0900

    Update index.js

--- boardfrontend/src/router/index.js
+++ boardfrontend/src/router/index.js
@@ -113,13 +113,18 @@ const routes = [
   },
   // 건의 게시판 -> 상세조회
   {
-    path: "/complaint/complaint-board/:complaintBoardId",
+    path: "/complaint/complaint-boardDetail/:complaintBoardId",
     component: () => import("../views/complaint/complaintDetail.vue"),
   },
   // 건의 게시판 -> 글쓰기
   {
     path: "/complaint/complaint-boardAdd",
     component: () => import("../views/complaint/complaintBoardAdd.vue"),
+  },
+  // 건의 게시판 -> 수정하기
+  {
+    path: "/complaint/complaint-board/Update/:complaintBoardId",
+    component: () => import("../views/complaint/complaintBoardUpdate.vue"),
   },
   // 각게시판별 공지사항 관리 ::
   {
