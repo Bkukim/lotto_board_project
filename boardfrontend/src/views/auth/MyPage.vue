80f16f40e90aedeab6c7907efdb2802b77a1bfa7
Bku <full1569@gmail.com>
2024-06-06 12:14:58 +0900

    Revert "Merge pull request #78 from Bkukim/JI-YOUNG"
    This reverts commit 536f1d3fd938d84ae87d5b45b2073d0a225daa02, reversing
    changes made to b770872948ef44cf6fc04bff8c72e7ffb109c88d.

--- boardfrontend/src/views/auth/MyPage.vue
+++ boardfrontend/src/views/auth/MyPage.vue
@@ -1,43 +1,21 @@
 <template>
-  <div style="height: auto">
-    <div>
-      <!-- <h2 class="text-center mb-5">My Page</h2> -->
+  <div class="mt-5" style="height: 900px">
+    <div >
+      <h2 class="text-center mb-5">My Page</h2>
 
       <div class="row">
         <!-- 왼쪽 사이드바 -->
-
-        <div
-          class="col-3 text-center text-lg-start sidebar"
-          style="background-color: #162b59"
-        >
-          <!-- <div class="text-center"
-            style="
-              width: 200px;
-              height: 200px;
-              border-radius: 50%;
-              background-color: #fff;
-              text-align: center;
-              margin: 50px 0 0 80px;
-            "
-          ></div> -->
-          <div class="circle"></div>
-
+        <div class="col-3 text-center text-lg-start">
           <ul
             class="nav flex-column mt-5"
-            style="
-              gap: 50px;
-              font-size: 25px;
-              border-right: 3px solid #162b59;
-              height: 900px;
-              padding-top: 100px;
-              text-align: center;
-            "
+            style="gap: 50px; font-size: 25px; border-right: 3px solid #162b59; height: 700px; padding-top: 50px;
+            text-align: center;"
           >
             <li class="nav-item">
               <a
                 class="nav-link"
                 href="#"
-                style="border-bottom: 1px solid #cccccc; color: #fff"
+                style="border-bottom: 1px solid #cccccc; color: #555555;"
                 @click="showProfile"
                 >프로필</a
               >
@@ -46,33 +24,24 @@
               <a
                 class="nav-link"
                 href="#"
-                style="border-bottom: 1px solid #cccccc; color: #fff"
+                style="border-bottom: 1px solid #cccccc; color: #555555;"
                 @click="showWrittenPosts"
                 >작성한 글</a
               >
             </li>
-            <!-- <li class="nav-item">
-              <a
-                class="nav-link"
-                href="/member/new-pw"
-                style="border-bottom: 1px solid #cccccc; color: #fff"
-                >비밀번호 변경</a
-              >
-            </li> -->
             <li class="nav-item">
               <a
                 class="nav-link"
-                href="#"
-                style="border-bottom: 1px solid #cccccc; color: #fff"
-                @click="ChangPassword"
-                >비밀번호 변경하기</a
+                href="/member/new-pw"
+                style="border-bottom: 1px solid #cccccc; color: #555555;"
+                >비밀번호 변경</a
               >
             </li>
             <li class="nav-item">
               <a
                 class="nav-link"
                 href="#"
-                style="border-bottom: 1px solid #cccccc; color: #fff"
+                style="border-bottom: 1px solid #cccccc; color: #555555;"
                 @click="showWithdrawal"
                 >회원탈퇴</a
               >
@@ -80,28 +49,8 @@
           </ul>
         </div>
 
-        <!-- ------------------------------- -->
-        <!-- 오른쪽 -->
         <!-- 화면 출력 -->
         <div class="col text-center">
-          <div
-            class="container text-center"
-            style="background-color: #f2f2f2; height: 1000px"
-          >
-            <div
-              class="container text-center"
-              style="
-                background-color: #fff;
-                width: 500px;
-                height: 700px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                border-radius: 10px;
-                margin-top: 10px;
-              "
-            >
-              엥
-            </div>
-          </div>
           <template v-if="displayedContent === 'profile'">
             <!-- 프로필 표시 -->
             <h3>프로필 화면</h3>
@@ -112,138 +61,6 @@
             <h3 class="mt-5">내가 작성한 글</h3>
             <!-- 작성한 글 목록 -->
           </template>
-          <template v-else-if="displayedContent === 'ChangPassword'">
-            <!-- 비밀번호 변경 표시 -->
-            <!-- <h3>비번번경</h3> -->
-            <div class="container">
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <h2 class="text-center">새로운 비밀번호 설정</h2>
-              <div class="container">
-                <div class="row justify-content-md-center" v-if="result">
-                  <div class="col-8">
-                    <div
-                      class="mt-5 text-center"
-                      id="box"
-                      style="height: 300px"
-                    >
-                      <div style="margin-top: 70px">
-                        <h3>비밀번호가 성공적으로 변경되었습니다.</h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row justify-content-md-center" v-else>
-                  <div class="col-8">
-                    <div class="mt-5" id="comment">
-                      <p>
-                        ＊새로운 비밀번호를 입력하세요.<br />
-                        ＊영문 대소문자/숫자/특수문자 중 3가지 이상 조합,
-                        8자~16자 이상으로 입력하세요.
-                      </p>
-                    </div>
-                    <div class="mt-5" id="box">
-                      <div class="mt-4" style="font-size: 20px">
-                        <label class="form-label">새로운 비밀번호</label>
-                        <input
-                          class="form-control"
-                          type="password"
-                          name="id"
-                          v-model="newPw"
-                          style="height: 50px"
-                          @input="checkPasswordMatch"
-                        />
-                      </div>
-
-                      <div style="margin-top: 70px; font-size: 20px">
-                        <label class="form-label">새로운 비밀번호 확인</label>
-                        <input
-                          class="form-control"
-                          type="password"
-                          name="pwdAskCheck"
-                          v-model="newPwCheck"
-                          style="height: 50px; margin-bottom: 50px"
-                          @input="checkPasswordMatch"
-                        />
-                      </div>
-                      <div class="mt-5 text-center">
-                        <p v-if="passwordMatchError" style="color: red">
-                          비밀번호가 일치하지 않습니다.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-3"></div>
-            </div>
-            <br />
-            <br />
-            <div class="container text-center" v-if="result">
-              <div class="row justify-content-md-center">
-                <div class="col-md-auto">
-                  <button
-                    class="text-light findPwdBtn btn-sm mt-4"
-                    id=""
-                    type="submit"
-                    @click="goLogin"
-                  >
-                    로그인 하러 가기
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="container text-center" v-else>
-              <div class="row justify-content-md-center">
-                <div class="col-md-auto">
-                  <button
-                    class="text-light findPwdBtn btn-sm"
-                    id=""
-                    type="submit"
-                    @click="updatePw"
-                  >
-                    확인
-                  </button>
-                </div>
-              </div>
-            </div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <!-- <div class="container text-center">
-    <div class="row justify-content-md-center">
-      <div class="col-md-auto">
-        <button
-          class="text-light FindIdBtn btn-sm mt-4"
-          id=""
-          type="submit"
-          @click="findId"
-        >
-          확인
-        </button>
-      </div>
-      <div class="col-md-auto">
-        <button
-          class="text-light btn-sm mt-4"
-          id="go-login"
-          type="submit"
-          @click="goLogin"
-         style="border: 1px solid #cccccc;"
-        >
-          <p>로그인</p>
-        </button>
-      </div>
-    </div>
-  </div> -->
-            <br />
-            <br />
-            <br />
-          </template>
           <template v-else-if="displayedContent === 'withdrawal'">
             <!-- 회원탈퇴 표시 -->
             <h3>회원탈퇴 화면</h3>
@@ -252,90 +69,28 @@
       </div>
     </div>
   </div>
-  <br />
-  <br />
-  <br />
-  <br />
-  <br />
-  <br />
 </template>
 
 <script>
-import AuthService from "@/services/auth/AuthService";
-
 export default {
   data() {
     return {
-      displayedContent: "", // 현재 화면에 표시할 내용을 저장하는 변수
-
-      // 비밀번호 확인이 같으면 true
-      passwordMatchError: false,
-
-      userId: this.$store.state.user.userId,
-      newPw: "",
-      newPwCheck: "",
-      result: false,
+      displayedContent: '', // 현재 화면에 표시할 내용을 저장하는 변수
     };
   },
   methods: {
     // 프로필 표시 메소드
     showProfile() {
-      this.displayedContent = "profile";
+      this.displayedContent = 'profile';
     },
     // 작성한 글 표시 메소드
     showWrittenPosts() {
-      this.displayedContent = "writtenPosts";
-    },
-    // 비밀번호 변경 글 표시 메소드
-    ChangPassword() {
-      this.displayedContent = "ChangPassword";
+      this.displayedContent = 'writtenPosts';
     },
     // 회원탈퇴 표시 메소드
     showWithdrawal() {
-      this.displayedContent = "withdrawal";
+      this.displayedContent = 'withdrawal';
     },
-
-    async updatePw() {
-      try {
-        let data = {
-          userId: this.userId,
-          newPw: this.newPw,
-        };
-        console.log(data);
-        if (this.newPw == this.newPwCheck) {
-          // 비밀번호 확인이 같을때만 실행
-          let response = await AuthService.updatePw(
-            this.$store.state.user.userId,
-            data
-          ); // 수정이 되면  true를 반환하고 안되면 false를 반환
-          if (response.data) {
-            this.$store.state.user.userId = "";
-            this.result = true;
-          } else {
-            alert("세션이 만료되었습니다. 로그인 창으로 이동합니다.");
-            this.$router.push("/member/login");
-          }
-        } else {
-          alert("비밀번호가 일치하지 않습니다.");
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    checkPasswordMatch: function () {
-      if (this.newPw !== this.newPwCheck) {
-        this.passwordMatchError = true;
-        this.passwordMatchError = true;
-      } else {
-        this.passwordMatchError = false;
-      }
-    },
-    goLogin() {
-      this.$router.push("/member/login");
-    },
-  },
-  mounted() {
-    window.scrollTo(0, 0);
   },
 };
 </script>
@@ -343,24 +98,5 @@ export default {
 <style>
 .nav-link:hover {
   font-weight: bold;
-}
-.sidebar {
-  background-color: #162b59;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 900px;
-}
-
-.circle {
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  background-color: #fff;
-  margin-top: 50px;
-  min-width: 200px;
-  min-height: 200px;
-  flex-direction: column;
-  align-items: center;
 }
 </style>
