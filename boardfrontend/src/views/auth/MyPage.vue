b3efa7abda0d5f1b5b62d949dfe474635f3172ba
wldudrhdwnsla99 <165975204+wldudrhdwnsla99@users.noreply.github.com>
2024-06-13 17:51:46 +0900

    지영이
    ㅎㅎ

--- boardfrontend/src/views/auth/MyPage.vue
+++ boardfrontend/src/views/auth/MyPage.vue
@@ -577,11 +577,7 @@ import AuthService from "@/services/auth/AuthService";
 import FreeBoardService from "@/services/board/free/FreeBoardService";
 import UserService from "@/services/user/UserService";
 import ComplaintBoardService from "@/services/board/complaint/ComplaintBoardService";
-import ClubBoardService from "@/services/board/club/ClubBoardService";
 import DeptBoardService from "@/services/board/dept/DeptBoardService";
-
-import ComplaintBoardService from "@/services/board/complaint/ComplaintBoardService";
-
 import ClubBoardService from '@/services/board/club/ClubBoardService';
 
 
